2953bcfa6d9d23603f4bbe543e344b43a8292d2c
sogod <szymon7127@gmail.com>
2024-09-08 22:39:57 +0200

    Add font and second menu improve

--- atlas_cwiczen/2-generatehtml-index.cjs
+++ atlas_cwiczen/2-generatehtml-index.cjs
@@ -19,6 +19,7 @@ fs.readFile("atlas.json", "utf8", (err, data) => {
         <title>Exercise Index</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         <link href="style.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Display&display=swap" rel="stylesheet">
     </head>
     <body>
         <div id="top-menu">
