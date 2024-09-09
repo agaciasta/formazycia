37e9b7ebc2c80bf0cdc3a6039bc85c55d1746406
sogod <szymon7127@gmail.com>
2024-09-09 19:00:24 +0200

    Adding bootstrap to nav bar and change grid to bootstrap

--- atlas_cwiczen/2-generatehtml-index.cjs
+++ atlas_cwiczen/2-generatehtml-index.cjs
@@ -22,22 +22,36 @@ fs.readFile("atlas.json", "utf8", (err, data) => {
         <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Display&display=swap" rel="stylesheet">
     </head>
     <body>
-        <div id="top-menu">
-            <div id="menu-links">`
+      <nav class="navbar navbar-expand-lg bg-body-tertiary" id="top-menu">
+          <div class="container-fluid">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+          </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0"> `
             
     for (category in exercisesData)
     {
       const sectionId = category.trim().toLowerCase().replace(/\s+/g, '-');
 
-        htmlContent += `<a href="#${sectionId}">${category}</a>`;
+        htmlContent += `
+        <li class="nav-item">
+        <a class="nav-link" href="#${sectionId}">${category}</a>
+        </li>`;
     }
             
-    htmlContent+= `</div>
-            <input type="text" id="search" placeholder="Search exercises..." autocomplete="off" autofill="off" autocorrect="off" spellcheck="false">
-        </div>
+    htmlContent+= `
+    </ul>
+
+            <input type="text" class="me-2" id="search" placeholder="Search exercises..." autocomplete="off" autofill="off" autocorrect="off" spellcheck="false">
+            </div>
+            </nav>
         <div id="exercise-list">
     `;
 
+
+
+    
     let basePreviewNumber = 54839;
   // Add exercises to the HTML content
   for (const category in exercisesData) {
@@ -47,7 +61,7 @@ fs.readFile("atlas.json", "utf8", (err, data) => {
     htmlContent += `
             <div class="section-container" id="${sectionId}">
             <h2 class="section-title">${category}</h2> 
-            <div class="atlas-cwiczen-grid-container">
+            <div class="row row-cols-1 row-cols-md-3 row-cols-xl-4 ms-5 me-5 text-center">
             `;
 
     for (const exercise in exercisesData[category]) {
@@ -56,8 +70,8 @@ fs.readFile("atlas.json", "utf8", (err, data) => {
 
       const previewUrl = `https://skyier.com/home/courses/5615/preview/${basePreviewNumber}`;
       htmlContent += `
-            <a class="card-link exercise" data-name="${exercise.toLowerCase()}" href='https://skyier.com/home/courses/5615/preview/${basePreviewNumber++}' target="_parent"> 
-            <div class="atlas-cwiczen-grid-item col-md-4 mb-4">
+            <a class="card-link exercise text-decoration-none" data-name="${exercise.toLowerCase()}" href='https://skyier.com/home/courses/5615/preview/${basePreviewNumber++}' target="_parent"> 
+            <div class="col mb-4">
                 <div class="card">
                     <img src="${jpg}" class="card-img-top" alt="${exercise}">
                     <div class="card-body">
